bbd5c5eda2e4d8e4ed9c7295334f29ef2b7d6d2a
Carlos Coves Prieto <escorponox@gmail.com>
2017-03-23 13:39:04 +0100

    heights almost working

--- app/scripts/layer-animations.js
+++ app/scripts/layer-animations.js
@@ -1,82 +1,92 @@
-import slideUp from './slide-up';
+import slide from './slide'
 
-const labels = Array.from(document.querySelectorAll('.c-layer__label'));
-const layerContainers = Array.from(document.querySelectorAll('.c-layers'));
+const labels = Array.from(document.querySelectorAll('.c-layer__label'))
+const layerContainers = Array.from(document.querySelectorAll('.c-layers'))
 
-const calculateLayerHeight = layer => Array.from(layer.children)
-  .reduce((total, curr) => curr.offsetHeight + total, 2);
+const calculateLayerHeight = layer => {
+  const frontContentHeight = Array.from(layer.children)
+    .reduce((total, curr) => curr.offsetHeight + total, 2)
+  return frontContentHeight < 480 ? 480 : frontContentHeight
+}
 
-const calculateContainerHeight = container => Array.from(container.querySelectorAll('.c-layer'))
-  .reduce((total, curr) => curr.classList.contains('c-layer--selected')
-      ? calculateLayerHeight(curr) + total
-      : curr.querySelector('.c-layer__label').offsetHeight + 1 + total
-    , 0);
+const calculateContainerHeight = layer => {
+  return Array.from(layer.parentNode.querySelectorAll('.c-layer'))
+    .reduce((total, curr) => curr === layer
+        ? calculateLayerHeight(curr) + total
+        : curr.querySelector('.c-layer__label').offsetHeight + 1 + total
+      , 0)
+}
 
-const animateFrontLayer = layer => slideUp(layer, 480, 300)
+const animateFrontLayer = layer => slide(layer, 480, 300)
   .then(layer => new Promise((resolve) => {
-    layer.classList.add('c-layer--hide');
-    setTimeout(() => resolve(layer), 200)
+    setTimeout(() => {
+      layer.classList.add('c-layer--hide', 'c-layer--visited')
+      resolve(layer)
+    }, 200)
   }))
   .then(layer => new Promise((resolve) => {
-    const previousLayer = layer.previousElementSibling;
-    layer.parentNode.insertBefore(layer, layer.parentNode.firstElementChild);
-    resolve(previousLayer);
-  }));
+    const previousLayer = layer.previousElementSibling
+    layer.parentNode.insertBefore(layer, layer.parentNode.firstElementChild)
+    resolve(previousLayer)
+  }))
 
 const animateMiddleLayers = (newFrontLayer, layer) => {
   if (newFrontLayer === layer) {
-    return newFrontLayer;
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(newFrontLayer)
+      }, 500)
+    })
   }
 
   return new Promise((resolve) => {
-    layer.classList.add('c-layer--hide');
+    layer.classList.add('c-layer--hide')
     setTimeout(() => {
-      const previousLayer = layer.previousElementSibling;
-      layer.parentNode.insertBefore(layer, layer.parentNode.firstElementChild);
-      resolve(animateMiddleLayers(newFrontLayer, previousLayer));
-    }, 200);
-  });
-};
-
-const showFrontLayer = layer => {
-
-};
+      const previousLayer = layer.previousElementSibling
+      layer.parentNode.insertBefore(layer, layer.parentNode.firstElementChild)
+      resolve(animateMiddleLayers(newFrontLayer, previousLayer))
+    }, 500)
+  })
+}
 
 const moveForward = layer => {
   Array.from(layer.parentNode.querySelectorAll('.c-layer__label'))
-    .forEach(label => label.addEventListener('click', moveForwardListener));
+    .forEach(label => label.addEventListener('click', moveForwardListener))
 
   animateFrontLayer(layer.parentNode.lastElementChild)
     .then(animateMiddleLayers.bind(null, layer))
     .then(layer => {
-      layer.classList.add('c-layer--selected');
-      layer.style.height = calculateLayerHeight(layer);
-      layer.parentNode.style.height = calculateContainerHeight(layer.parentNode);
+      const frontHeight = calculateLayerHeight(layer)
+      const containerHeight = calculateContainerHeight(layer)
+      return Promise.all([slide(layer, frontHeight, 500, true), slide(layer.parentNode, containerHeight, 500)])
+    })
+    .then(data => {
+      console.log(data)
+      return data
+    })
+    .then(layers => {
+      Array.from(layers[0].parentNode.querySelectorAll('.c-layer--hide'))
+        .forEach(hiddenLayer => hiddenLayer.classList.remove('c-layer--hide'))
       return layer
     })
     .then(layer => {
-      Array.from(layer.parentNode.querySelectorAll('.c-layer--hide'))
-        .forEach(hiddenLayer => hiddenLayer.classList.remove('c-layer--hide'));
-      return layer;
-    })
-    .then(layer => {
       Array.from(layer.parentNode.querySelectorAll('.c-layer__label'))
-        .forEach(label => label.addEventListener('click', moveForwardListener));
-    });
-};
+        .forEach(label => label.addEventListener('click', moveForwardListener))
+    })
+}
 
 const moveForwardListener = event => {
-  const layer = event.target.parentNode;
+  const layer = event.target.parentNode
   if (!layer.classList.contains('c-layer--selected')) {
-    moveForward(layer);
+    moveForward(layer)
   }
-};
+}
 
 export default () => {
-  labels.forEach(label => label.addEventListener('click', moveForwardListener));
-  layerContainers.forEach( layerContainer => {
-    layerContainer.style.height = calculateContainerHeight(layerContainer);
-    const selected = layerContainer.querySelector('.c-layer--selected');
-    selected.style.height = calculateLayerHeight(selected);
+  labels.forEach(label => label.addEventListener('click', moveForwardListener))
+  layerContainers.forEach(layerContainer => {
+    const selected = layerContainer.querySelector('.c-layer--selected')
+    selected.style.height = calculateLayerHeight(selected)
+    layerContainer.style.height = calculateContainerHeight(selected)
   })
 }
---
+++ app/scripts/slide.js
@@ -0,0 +1,35 @@
+export default (layer, endHeight, duration, down = false) => new Promise((resolve, reject) => {
+  let start = null
+  const initHeight = layer.offsetHeight
+  if (layer.classList.contains('c-layer')) {
+    down ? layer.classList.add('c-layer--selected') : layer.classList.remove('c-layer--selected')
+  }
+
+  function step (timestamp) {
+    if (!start) start = timestamp
+    const progress = timestamp - start
+    layer.style.height = down
+      ? easingDown(progress, initHeight, endHeight - initHeight, duration)
+      : easing(progress, initHeight, endHeight - initHeight, duration)
+    if (progress < duration) {
+      window.requestAnimationFrame(step)
+    } else {
+      endAnimation()
+    }
+  }
+
+  // easeOutCubic - George McGinley Smith - https://github.com/danro/jquery-easing/blob/master/jquery.easing.js
+  function easing (t, b, c, d) {
+    return c * ((t = t / d - 1) * t * t + 1) + b
+  }
+
+  function easingDown (t, b, c, d) {
+    return c * (t /= d) * t * t + b
+  }
+
+  function endAnimation () {
+    resolve(layer)
+  }
+
+  window.requestAnimationFrame(step)
+})
