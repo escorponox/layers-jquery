a264e8cb28e0ac9e4520ee33513bf6ac3be6347d
Carlos Coves Prieto <escorponox@gmail.com>
2017-03-29 11:04:20 +0200

    border fixes

--- app/scripts/layer-animations.js
+++ app/scripts/layer-animations.js
@@ -5,14 +5,14 @@ const layerContainers = Array.from(document.querySelectorAll('.c-layers'))
 
 const calculateLayerHeight = layer => {
   const contentHeight = Array.from(layer.children)
-    .reduce((total, curr) => curr.offsetHeight + total, 4)
+    .reduce((total, curr) => curr.offsetHeight + total, 2)
   return contentHeight < 480 ? 480 : contentHeight
 }
 
 const calculateContainerHeight = layer => Array.from(layer.parentNode.querySelectorAll('.c-layer'))
   .reduce((total, curr) => curr === layer
       ? calculateLayerHeight(curr) + total
-      : curr.querySelector('.c-layer__label').offsetHeight + 2 + total
+      : curr.querySelector('.c-layer__label').offsetHeight + 1 + total
     , 0)
 
 const staggeredBackwards = (backwardsLayers, previousLayer) => {
