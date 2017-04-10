cf7059961e3d091ea9c66b1d47a3fd5e2c8af398
Carlos Coves Prieto <escorponox@gmail.com>
2017-04-10 13:21:21 +0200

    Update 2017-04-10T11:21:19.295Z

--- README.md
+++
@@ -1,5 +0,0 @@
-# LAYERS
-
-Layers component
-
-Developed with [Frontend Init](https://cusxio.github.io/frontend-init/)
--- app/scripts/document-ready.js
+++
@@ -1,5 +0,0 @@
-const documentReady = (cb) => {
-  document.readyState === "interactive" || document.readyState === "complete" ? cb() : document.addEventListener("DOMContentLoaded", cb);
-};
-
-export default documentReady;
